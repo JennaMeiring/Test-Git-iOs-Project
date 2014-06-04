9a518d0b80ee6c62671afc3ba1d26c9026a83291
Alex Shiells <macbookair@Alex.local>
2014-06-04 16:17:14 +0200

    added a test class

---
+++ AJTest.h
@@ -0,0 +1,13 @@
+//
+//  AJTest.h
+//  GitTest
+//
+//  Created by Alex Shiells on 2014/06/04.
+//  Copyright (c) 2014 Absol. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AJTest : NSObject
+
+@end
---
+++ AJTest.m
@@ -0,0 +1,13 @@
+//
+//  AJTest.m
+//  GitTest
+//
+//  Created by Alex Shiells on 2014/06/04.
+//  Copyright (c) 2014 Absol. All rights reserved.
+//
+
+#import "AJTest.h"
+
+@implementation AJTest
+
+@end
